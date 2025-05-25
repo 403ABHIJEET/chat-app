d8ffee6ce204498acacb14624ba70fe6017a1843
403ABHIJEET <abhijeet8745@gmail.com>
2025-05-25 09:17:50 +0530

    backend message got corrected

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -51,7 +51,7 @@ export async function GET(request: NextRequest) {
             const user = await UserModel.findById(userId)
             return NextResponse.json({
                 success: true,
-                message: "Users fetched successfully.",
+                message: "User fetched successfully.",
                 data: user
             }, {status: 200})
         }
