629b684e321c77f3ef0e960e3af751fae2725092
ABHIJEET <abhijeet.kumar@avaloninfosys.com>
2025-06-05 17:18:17 +0530

    bug solved by yashwin of Ui

--- src/components/hero-section-demo-1.tsx
+++ src/components/hero-section-demo-1.tsx
@@ -22,7 +22,7 @@ export default function HeroSectionOne() {
       </div>
 
       {/* Hero Content */}
-      <div className="px-4 py-10 md:py-20">
+      <div className="px-4 py-10 md:py-20 w-full">
         <h1 className="z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
           {"Connect instantly. Chat seamlessly. Anytime, anywhere."
             .split(" ")
@@ -57,7 +57,7 @@ export default function HeroSectionOne() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3, delay: 1 }}
-          className="w-fit z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+          className="w-full z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             Try Chat Now
@@ -68,12 +68,12 @@ export default function HeroSectionOne() {
         </motion.div>
 
         {/* Preview Image */}
-        <div className="flex justify-center">
+        <div className="flex justify-center overflow-hidden">
           <motion.div
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3, delay: 1.2 }}
-            className="w-1/2 overflow-hidden relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
+            className="w-full overflow-hidden relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
           >
             <div className="rounded-xl border border-gray-300 dark:border-gray-700">
               <MarqueeDemo />
