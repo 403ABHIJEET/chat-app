421f191aa534b686f31ececfef03259304067962
403ABHIJEET <abhijeet8745@gmail.com>
2025-07-06 10:09:02 +0530

    making compatible for local

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,10 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://chat-app-backend-ykcc.onrender.com', {
+const socket = io('192.168.1.2:5000', {
   transports: ['websocket', 'polling'],
-  secure: true,
   reconnection: true,
-  rejectUnauthorized: false
 });
 
 export default socket;
